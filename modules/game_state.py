f221c76722aeb78e0f3b5ec286d007fe9a3aa4a1
Đức Đào <duc.dao.431@gmail.com>
2024-01-29 09:28:03 +0700

    [game_state] hot fix GameState.__init__

--- modules/game_state.py
+++ modules/game_state.py
@@ -22,13 +22,13 @@ The game state class has the following methods:
 class GameState:
     def __init__(self, map, current_cost=0):
         self.map = map
+        self.current_cost = current_cost
+        self.height = len(self.map)
+        self.width = len(self.map[0])
         self.player = self.find_player()
         self.boxes = self.find_boxes()
         self.targets = self.find_targets()
         self.is_solved = self.check_solved()
-        self.current_cost = current_cost
-        self.height = len(self.map)
-        self.width = len(self.map[0])
 
     # ------------------------------------------------------------------------------------------------------------------
     # The following methods are used to find the player, boxes, and targets in the map
@@ -59,13 +59,13 @@ class GameState:
 
     def is_box(self, position):
         """Check if the given position is a box
-            Note: the box can be on "$" or "*" (box on target)
+        Note: the box can be on "$" or "*" (box on target)
         """
         pass
 
     def is_target(self, position):
         """Check if the given position is a target
-            Note: the target can be "." or "*" (box on target)
+        Note: the target can be "." or "*" (box on target)
         """
         pass
 
@@ -79,19 +79,19 @@ class GameState:
 
     def get_heuristic(self):
         """Get the heuristic for the game state
-            Note: the heuristic is the sum of the distances from all the boxes to their nearest targets
+        Note: the heuristic is the sum of the distances from all the boxes to their nearest targets
         """
         pass
 
     def get_total_cost(self):
         """Get the cost for the game state
-            Note: the cost is the number of moves from the initial state to the current state + the heuristic
+        Note: the cost is the number of moves from the initial state to the current state + the heuristic
         """
         pass
 
     def get_current_cost(self):
         """Get the current cost for the game state
-            Note: the current cost is the number of moves from the initial state to the current state
+        Note: the current cost is the number of moves from the initial state to the current state
         """
         pass
 
@@ -100,15 +100,15 @@ class GameState:
     # ------------------------------------------------------------------------------------------------------------------
 
     def move(self, direction):
-        """Generate the next game state by moving the player to the given direction. 
-            The rules are as follows:
-            - The player can move to an empty space
-            - The player can move to a target
-            - The player can push a box to an empty space (the box moves to the empty space, the player moves to the box's previous position)
-            - The player can push a box to a target (the box moves to the target, the player moves to the box's previous position)
-            - The player cannot move to a wall
-            - The player cannot push a box to a wall
-            - The player cannot push two boxes at the same time
+        """Generate the next game state by moving the player to the given direction.
+        The rules are as follows:
+        - The player can move to an empty space
+        - The player can move to a target
+        - The player can push a box to an empty space (the box moves to the empty space, the player moves to the box's previous position)
+        - The player can push a box to a target (the box moves to the target, the player moves to the box's previous position)
+        - The player cannot move to a wall
+        - The player cannot push a box to a wall
+        - The player cannot push two boxes at the same time
         """
         # TODO: implement this method
         return GameState(self.map, self.current_cost + 1)
